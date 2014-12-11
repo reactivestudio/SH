44bc2b14d05bbf8167e766b32931a233a930400d
reactivestudio <dima.bushin@gmail.com>
2014-12-12 03:50:13 +0700

    Add makefile

--- drupal-install.sh
+++ drupal-install.sh
@@ -35,6 +35,9 @@ jqueryVersion="1.7"
 modulesInstall=""
 ##########################################################
 
+# Drush .make
+##########################################################
+makefile="drupal-reactive-build.make"
 
 
 while [[ $# -gt 0 ]]; do
@@ -83,75 +86,76 @@ echo "Installing modules...";
 drush dl drush_language -y
 
 
-# Install config modules
-drush -y dl \
-adminimal_admin_menu \
-append_file_info \
-backup_migrate \
-devel \
-entity \
-ckeditor \
-ctools \
-fast_dblog \
-fences \
-field_collection_fieldset \
-field_group \
-filefield_paths \
-globalredirect \
-google_analytics \
-httprl \
-jquery_update \
-l10n_update \
-lightbox2 \
-menu_block \
-metatag \
-module_filter \
-pathauto \
-redirect \
-token \
-transliteration \
-views \
-views_bulk_operations \
-webform \
-xmlsitemap \
-yamaps;
+# # Install config modules
+# drush -y dl \
+# adminimal_admin_menu \
+# append_file_info \
+# backup_migrate \
+# devel \
+# entity \
+# ckeditor \
+# ctools \
+# fast_dblog \
+# fences \
+# field_collection_fieldset \
+# field_group \
+# filefield_paths \
+# globalredirect \
+# google_analytics \
+# httprl \
+# jquery_update \
+# l10n_update \
+# lightbox2 \
+# menu_block \
+# metatag \
+# module_filter \
+# pathauto \
+# redirect \
+# token \
+# transliteration \
+# views \
+# views_bulk_operations \
+# webform \
+# xmlsitemap \
+# yamaps;
 
 
-# Install useful modules
-drush -y en \
-adminimal_admin_menu \
-append_file_info \
-entity \
-ckeditor \
-ctools \
-fast_dblog \
-fences \
-field_collection_fieldset \
-field_group \
-filefield_paths \
-globalredirect \
-googleanalytics \
-httprl \
-image \
-jquery_update \
-l10n_update \
-list \
-lightbox2 \
-menu_block \
-metatag \
-module_filter \
-number \
-pathauto \
-redirect \
-text \
-token \
-transliteration \
-views \
-views_bulk_operations \
-webform \
-xmlsitemap \
-yamaps;
+# # Install useful modules
+# drush -y en \
+# adminimal_admin_menu \
+# append_file_info \
+# entity \
+# ckeditor \
+# ctools \
+# fast_dblog \
+# fences \
+# field_collection_fieldset \
+# field_group \
+# filefield_paths \
+# globalredirect \
+# googleanalytics \
+# httprl \
+# image \
+# jquery_update \
+# l10n_update \
+# list \
+# lightbox2 \
+# menu_block \
+# metatag \
+# module_filter \
+# number \
+# pathauto \
+# redirect \
+# text \
+# token \
+# transliteration \
+# views \
+# views_bulk_operations \
+# webform \
+# xmlsitemap \
+# yamaps;
 
+drush make --no-core --contrib-destination=$httpDir/$projectDir $makefile
 
 
 
